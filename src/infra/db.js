da659f7190eed1b34bd7d11c27a558cc2dae3c19
Flavio Henrique da Silva <firmezinha1@hotmail.com>
2025-08-29 11:10:37 -0300

    Update db.js

--- src/infra/db.js
+++ src/infra/db.js
@@ -4,9 +4,10 @@ const { Pool } = pkg
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: process.env.PG_SSL_REJECT_UNAUTHORIZED === 'true'
+    rejectUnauthorized: process.env.PG_SSL_REJECT_UNAUTHORIZED === true
   }
 })
 
 export default pool
+
 
